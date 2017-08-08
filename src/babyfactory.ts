12098499c15c0f536dba2f40925452c77e5d6c36
mind0n <weerspecial@126.com>
2017-08-08 18:19:20 +0800

    mesh test pass

--- src/babyfactory.ts
+++ src/babyfactory.ts
@@ -3,6 +3,7 @@ import {ModuleFactory, ModuleTemplate} from "web/modules/modulefactory";
 import {Module, NodeModule} from "web/modules/module";
 import {OperationNode} from "web/modules/operationode";
 import * as core from "common";
+import {BFrame} from "./test";
 
 export class BabyFactory extends ModuleFactory{
     static readonly instance:ModuleFactory = new BabyFactory();
@@ -34,10 +35,37 @@ export class WorldModule extends NodeModule{
         this.$obj = new BABYLON.Engine(node, true);
         let scope = <any>this.scope;
         scope.canvas = node;
+        let bf = new BFrame(this.scope.canvas, this.$obj);
+        scope.bf = bf;
     }
     dorender():string{
         let html = `<canvas alias="cvs" class="b-world"></canvas>`;
         return html;
+    }
+    onready(){
+        //console.log(this.scope);
+        this.animate();
+
+        // let bf = this.scope.bf;
+        // console.log(this.scope);
+        // bf.createScene(
+        //     this.scope.activeScene
+        //     , this.scope.activeCamera
+        // );
+        // bf.animate();
+    }
+    protected animate() : void {
+        let self = this;
+        // run the render loop
+        this.Engine.runRenderLoop(() => {
+            //this._scene.render();
+            self.scope.activeScene.render();
+        });
+
+        // the canvas/window resize event handler
+        window.addEventListener('resize', () => {
+            this.Engine.resize();
+        });
     }
 }
 
@@ -55,6 +83,8 @@ export class SceneModule extends Module{
         let engine = <BABYLON.Engine>world.$obj;
         let scene = new BABYLON.Scene(engine);
         this.$obj = scene;
+        this.scope.activeScene = scene;
+        //console.log(this.scope);
     }
 }
 
@@ -69,14 +99,17 @@ export class CameraModule extends Module{
 
     onready(parent:Module){
         let scene = <SceneModule>parent;
+        //let camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene.$obj);
         let camera = <BABYLON.TargetCamera>bcreate(this.$props.type, ['camera1', new BABYLON.Vector3(0, 5,-10), scene.$obj]);
         let target = eval(this.$props.target);
         let v3 = BABYLON.Vector3.FromArray(target);
         camera.setTarget(v3);
         if (this.$props.active){
+            console.log(this.scope.canvas);
             camera.attachControl(this.scope.canvas);
         }
         this.$obj = camera;
+        this.scope.activeCamera = camera;
     }
 }
 
@@ -91,13 +124,45 @@ export class LightModule extends Module{
 
     onready(parent:Module){
         let scene = <SceneModule>parent;
-        let light = bcreate(this.$props.type, ['light1', new BABYLON.Vector3(0,1,0), scene.$obj]);
+        let light = bcreate(this.$props.type, [this.$props.name, new BABYLON.Vector3(0,1,0), scene.$obj]);
         this.$obj = light;
+    }
+}
+
+export class MeshModule extends Module{
+    constructor(){
+        super('mesh');
+    }
+
+    create(){
+        return new MeshModule();
+    }
+
+    onready(parent:Module){
+        let scene = <SceneModule>parent;
+        let options = eval(`(${this.$props.options})`);
+        let mesh = bmesh(this.$props.type, [this.$props.name, options, scene.$obj]);
+        
+        this.$obj = mesh;
     }
 }
 
 function bcreate(type:string, args?:any[]){
     let w = <any>window;
-    let rlt = new (Function.prototype.bind.apply(w['BABYLON'][type], args));
+    return applyToConstructor(w['BABYLON'][type], args);
+    //console.log(w['BABYLON'][type]);
+    //let rlt = new w['BABYLON'][type].apply(null, args);
+    //return (rlt);
+}
+
+function applyToConstructor(constructor:any, argArray:any) {
+    var args = [null].concat(argArray);
+    var factoryFunction = constructor.bind.apply(constructor, args);
+    return new factoryFunction();
+}
+
+function bmesh(type:string, args?:any[]){
+    let w = <any>window;
+    let rlt = <BABYLON.Mesh>w['BABYLON']['MeshBuilder'][`Create${type}`].apply(null, args);
     return rlt;
 }
--- src/bframe.ts
+++ src/bframe.ts
@@ -8,6 +8,7 @@ export function init(){
     BF.BabyFactory.instance.regist(new BF.SceneModule());
     BF.BabyFactory.instance.regist(new BF.LightModule());
     BF.BabyFactory.instance.regist(new BF.CameraModule());
+    BF.BabyFactory.instance.regist(new BF.MeshModule());
     Noder.instance.regist(BF.BabyFactory.instance);
     Noder.instance.parse(document.body);
     //let bframe = new BFrame(document.body);
@@ -18,63 +19,5 @@ export function init(){
     // start animation
     //bframe.animate();
     info.log("Bframe module loaded");
-}
-
-class BFrame {
-    private _canvas: HTMLCanvasElement;
-    private _engine: BABYLON.Engine;
-    private _scene: BABYLON.Scene;
-    private _camera: BABYLON.FreeCamera;
-    private _light: BABYLON.Light;
-
-    constructor(entry : any) {
-        // Create canvas and engine
-        let entryEl:Element = entry;
-        if (typeof(entry) == 'string'){
-            entryEl = document.querySelector(entry);
-        }
-        // this._canvas = <HTMLCanvasElement>document.getElementById(entry);
-        // this._engine = new BABYLON.Engine(this._canvas, true);
-    }
-
-    createScene() : void {
-        // create a basic BJS Scene object
-        this._scene = new BABYLON.Scene(this._engine);
-
-        // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this._scene);
-
-        // target the camera to scene origin
-        this._camera.setTarget(BABYLON.Vector3.Zero());
-
-        // attach the camera to the canvas
-        this._camera.attachControl(this._canvas, false);
-
-        // create a basic light, aiming 0,1,0 - meaning, to the sky
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
-
-        // create a built-in "sphere" shape; with 16 segments and diameter of 2
-        let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
-                                    {segments: 16, diameter: 2}, this._scene);
-
-        // move the sphere upward 1/2 of its height
-        sphere.position.y = 1;
-
-        // create a built-in "ground" shape
-        let ground = BABYLON.MeshBuilder.CreateGround('ground1',
-                                    {width: 6, height: 6, subdivisions: 2}, this._scene);
-    }
-
-    animate() : void {
-        // run the render loop
-        this._engine.runRenderLoop(() => {
-            this._scene.render();
-        });
-
-        // the canvas/window resize event handler
-        window.addEventListener('resize', () => {
-            this._engine.resize();
-        });
-    }
 }
 
